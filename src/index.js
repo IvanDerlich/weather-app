47b428123cdcc196a5d19c945ffc6c1b46df3e7d
ivanderlich@gmail.com <ivanderlich@gmail.com>
2020-05-09 06:50:14 -0300

    Correct response from server

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-
-const url = "http://api.openweathermap.org/data/2.5/weather?q=London";
+// eslint-disable-next-line max-len
+const url = "http://api.openweathermap.org/data/2.5/weather?q=London&appid=b93ac565c07c898f8ab078f813afa920";
 
 
 console.log("Helloworld! Deployed deplyed deployed");
 console.log("Another deploy")
 
-
-fetch(url).
-    then((response) => {
-
-        console.log(response);
-    })
-    .catch((err) => {
-
-        console.log(err);
-
-    });
+fetch(url)
+.then((response) => {
+    return response.json();
+})
+.then( (response) =>{
+    console.log(response)
+})
+.catch((err) => {
+    console.log(err);
+});
