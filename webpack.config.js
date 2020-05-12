a89d88af005866af81e5f902de40a4a73d3b2be9
ivanderlich@gmail.com <ivanderlich@gmail.com>
2020-05-12 16:19:26 -0300

    Commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,73 @@
+/* eslint-disable prefer-named-capture-group */
+/* eslint-disable sort-keys */
+/* eslint-disable require-unicode-regexp */
 /* eslint-disable no-undef */
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 
-var config = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[hash].[ext]',
-            outputPath: '',
-          },
-        },
-      },
-      {
-        test: /\.html$/i,
-        loader: 'html-loader',
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Output Management',
-      "template": './src/index.html',
-        }),
-    ],
+config = {
+    "entry": "./src/index.js",
+    "module": {
+        "rules": [
+            {"test": /\.js$/,
+                "exclude": /node_modules/,
+                "loader": "babel-loader"},
+            {
+                "test": /\.scss$/,
+                "use": [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            },
+            {
+                "test": /\.(jpe?g|png|gif|svg)$/i,
+                "use": {
+                    "loader": "file-loader",
+                    "options": {
+                        "name": "[name].[hash].[ext]",
+                        "outputPath": ""
+                    }
+                }
+            },
+            {
+                "test": /\.html$/i,
+                "loader": "html-loader"
+            }
+        ]
+    },
+    "output": {
+        "path": path.resolve(
+            __dirname,
+            "dist"
+        )
+    },
+    "plugins": [
+        new HtmlWebpackPlugin({
+            "title": "Output Management",
+            "template": "./src/index.html"
+        })
+    ]
 };
 
 module.exports = (env, argv) => {
-  if (argv.mode === 'development') {
-    config.devtool = 'source-map';
-    config.output.filename = 'main.bundle.js';
-  }
 
-  if (argv.mode === 'production') {
-    config.plugins.push(new CleanWebpackPlugin());
-    config.output.filename = 'main.[hash].bundle.js';
-  }
-  return config;
+    if (argv.mode === "development") {
+
+        config.devtool = "source-map";
+        config.output.filename = "main.bundle.js";
+
+    }
+
+    if (argv.mode === "production") {
+
+        config.plugins.push(new CleanWebpackPlugin());
+        config.output.filename = "main.[hash].bundle.js";
+
+    }
+
+    return config;
+
 };
