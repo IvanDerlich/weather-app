8aba9cefcb9f5a026d9570f28deffc964eeffa24
ivanderlich@gmail.com <ivanderlich@gmail.com>
2020-05-13 15:25:59 -0300

    Fix Review

--- src/index.js
+++ src/index.js
@@ -1,65 +1,65 @@
-/* eslint-disable func-style */
-import "./style.scss"
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import './style.scss';
 
-// eslint-disable-next-line max-len
 const
-    cityInput = document.querySelector("#city"),
-    cityLabel = document.querySelector("#city-label"),
-    errorDiv = document.querySelector("#error"),
-    feelsDiv = document.querySelector("#feels"),
-    humidityDiv = document.querySelector("#humidity"),
-    tempDiv = document.querySelector("#temp"),
-    // eslint-disable-next-line max-len
-    url = "http://api.openweathermap.org/data/2.5/weather?appid=b93ac565c07c898f8ab078f813afa920&units=metric",
-    windsDiv = document.querySelector("#winds")
+  cityInput = document.querySelector('#city');
+const cityLabel = document.querySelector('#city-label');
+const errorDiv = document.querySelector('#error');
+const feelsDiv = document.querySelector('#feels');
+const humidityDiv = document.querySelector('#humidity');
+const tempDiv = document.querySelector('#temp');
+const url = 'http://api.openweathermap.org/data/2.5/weather?appid=b93ac565c07c898f8ab078f813afa920&units=metric';
+const windsDiv = document.querySelector('#winds');
 
+function errorHandling(err) {
+  errorDiv.innerHTML = `Error: ${err}`;
+  // eslint-disable-next-line no-multi-assign
+  tempDiv.innerHTML = windsDiv.innerHTML = humidityDiv.innerHTML = feelsDiv.innerHTML = '';
+}
 
-// eslint-disable-next-line require-jsdoc
-function get (city) {
+function get(city) {
+  windsDiv.innerHTML = '';
+  humidityDiv.innerHTML = '';
+  feelsDiv.innerHTML = '';
+  cityLabel.innerHTML = `City: ${cityInput.value}`;
+  tempDiv.innerHTML = 'Fetching data...';
+  const queryUrl = `${url}&q=${city}`;
 
-    windsDiv.innerHTML = "";
-    humidityDiv.innerHTML = "";
-    feelsDiv.innerHTML = "";
-    cityLabel.innerHTML = cityInput.value;
-    tempDiv.innerHTML = "Fetching data..."
-    const queryUrl = `${url}&q=${city}`;
+  fetch(queryUrl)
+    .then((response) => response.json())
+    .then((response) => {
+      try {
+        if (response.cod !== 200) throw response.message;
+        if (response.main.temp) tempDiv.innerHTML = `Temperature: ${response.main.temp} ºC`;
 
-    fetch(queryUrl).
-        then((response) => response.json()).
-        then((response) => {
+        if (response.wind.speed && response.wind.deg) {
+          windsDiv.innerHTML = `Winds: ${response.wind.speed
+          } m/s from ${response.wind.deg}º`;
+        }
 
-            console.log(response)
-            tempDiv.innerHTML = `Temperature: ${response.main.temp} ºC`
-            windsDiv.innerHTML = `Winds: ${response.wind.speed
-            } m/s from ${response.wind.deg}º`;
-            humidityDiv.innerHTML = `Humidity: ${response.main.humidity} %`
-            feelsDiv.innerHTML = `Feels: ${response.main.feels_like} ºC`
-            cityLabel.innerHTML = cityInput.value;
-            errorDiv.innerHTML = "";
+        if (response.main.humidity) humidityDiv.innerHTML = `Humidity: ${response.main.humidity} %`;
 
-        }).
-        catch((err) => {
+        if (response.main.feels_like) feelsDiv.innerHTML = `Feels: ${response.main.feels_like} ºC`;
 
-            errorDiv.innerHTML = err;
-            console.log(err)
-
-        });
-
+        errorDiv.innerHTML = '';
+      } catch (err) {
+        errorHandling(err);
+      }
+    })
+    .catch((err) => {
+      errorHandling(err);
+    });
 }
 
 window.onload = () => {
-
-    cityInput.click()
-
-}
+  cityInput.click();
+};
 
 cityInput.addEventListener(
-    "keyup",
-    () => {
+  'keyup',
+  () => {
+    const city = cityInput.value;
 
-        const city = cityInput.value;
-
-        get(city);
-
-    }
-)
+    get(city);
+  },
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,65 @@
-/* eslint-disable prefer-named-capture-group */
-/* eslint-disable sort-keys */
-/* eslint-disable require-unicode-regexp */
-/* eslint-disable no-undef */
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 
-config = {
-    "entry": "./src/index.js",
-    "module": {
-        "rules": [
-            {"test": /\.js$/,
-                "exclude": /node_modules/,
-                "loader": "babel-loader"},
-            {
-                "test": /\.scss$/,
-                "use": [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader"
-                ]
-            },
-            {
-                "test": /\.(jpe?g|png|gif|svg)$/i,
-                "use": {
-                    "loader": "file-loader",
-                    "options": {
-                        "name": "[name].[hash].[ext]",
-                        "outputPath": ""
-                    }
-                }
-            },
-            {
-                "test": /\.html$/i,
-                "loader": "html-loader"
-            }
-        ]
-    },
-    "output": {
-        "path": path.resolve(
-            __dirname,
-            "dist"
-        )
-    },
-    "plugins": [
-        new HtmlWebpackPlugin({
-            "title": "Output Management",
-            "template": "./src/index.html"
-        })
-    ]
+const config = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]',
+            outputPath: '',
+          },
+        },
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(
+      __dirname,
+      'dist',
+    ),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/index.html',
+    }),
+  ],
 };
 
 module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    config.devtool = 'source-map';
+    config.output.filename = 'main.bundle.js';
+  }
 
-    if (argv.mode === "development") {
+  if (argv.mode === 'production') {
+    config.plugins.push(new CleanWebpackPlugin());
+    config.output.filename = 'main.[hash].bundle.js';
+  }
 
-        config.devtool = "source-map";
-        config.output.filename = "main.bundle.js";
-
-    }
-
-    if (argv.mode === "production") {
-
-        config.plugins.push(new CleanWebpackPlugin());
-        config.output.filename = "main.[hash].bundle.js";
-
-    }
-
-    return config;
-
+  return config;
 };
